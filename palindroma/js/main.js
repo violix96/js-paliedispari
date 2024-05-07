95da762eadb951cbc192d33a173cf2f279abd98a
Vincenzo Fabrizio Viola <vincy1621@gmail.com>
2024-05-07 17:03:48 +0200

    [UPD] end of the exercise

--- palindroma/js/main.js
+++ palindroma/js/main.js
@@ -2,3 +2,26 @@
 // reset js 
 
 console.log('ciao mondo');
+
+// input tramite prompt all'utente per linserimento della parola 
+
+let parolaUtante = prompt('Inserisci una parola:');
+
+// funzione per verificare se la parola è palindroma
+function palindroma(parola){
+
+    let parolaInversa = parola.split('').reverse().join('');
+
+    if(parolaInversa === parola){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+//  stampa del risultato
+if(palindroma(parolaUtante)){
+    console.log('La parola è palindroma');
+}else{
+    console.log('La parola non è palindroma');
+}
